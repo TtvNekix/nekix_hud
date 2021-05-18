8ce7645a741433cc09c9799ae24c3ebeede6c250
Juan <69590591+TtvNekix@users.noreply.github.com>
2021-05-19 01:26:45 +0200

    Delete nek_hud directory

--- nek_hud/server/main.lua
+++
@@ -1,8 +0,0 @@
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-RegisterNetEvent('server:system:getHour')
-AddEventHandler('server:system:getHour', function()
-    local hour = os.date("%H:%M")
-    TriggerClientEvent('client:system:getHour', source, hour)
-end)
