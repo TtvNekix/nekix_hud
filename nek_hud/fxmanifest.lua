efa2eb164273b3242a5658f402dcc1e3690a8661
Juan <69590591+TtvNekix@users.noreply.github.com>
2021-05-19 01:44:22 +0200

    Add files via upload

---
+++ nek_hud/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+shared_scripts { 'config/*.lua' }
+client_scripts { 'client/*.lua' }
+ui_page 'html/index.html'
+server_scripts { 'server/*.lua' }
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
---
+++ nek_hud/html/script.js
@@ -0,0 +1,89 @@
+var EnableJobHTML = true;
+var EnableHourHTML = true;
+var EnableIdHTML = true;
+var EnablePlayerHTML = true;
+
+window.addEventListener('message', (event) => {
+    let jobPlayer = event.data.playerJob;
+    let hourPlayer = event.data.playerHour;
+    let idPlayer = event.data.playerId;
+    let healthPlayer = event.data.playerHealth;
+    let hungherPlayer = event.data.playerHungher;
+    let thirstPlayer = event.data.playerThirst;
+    let stressPlayer = event.data.playerStress;
+    let jobHTML = document.getElementById("job");
+    let hourHTML = document.getElementById("hour");
+    let idHTML = document.getElementById("id");
+    let healthHTML = document.getElementById("health");
+    let hungherHTML = document.getElementById("hungher");
+    let thirstHTML = document.getElementById("thirst");
+    let stressHTML = document.getElementById("stress");
+
+    if(event.data.jobValue === true) {
+        EnableJobHTML = true;
+    } else if(event.data.jobValue === false) {
+        EnableJobHTML = false;
+    }
+
+    if(EnableJobHTML === true) {
+        jobHTML.style.opacity = 1;
+        jobHTML.innerHTML = jobPlayer;
+    } else if(EnableJobHTML === false) {
+        jobHTML.style.opacity = 0;
+    }
+
+    if(event.data.playerValue === true) {
+        EnablePlayerHTML = true;
+    } else if(event.data.playerValue === false) {
+        EnablePlayerHTML = false;
+    }
+
+    if(EnablePlayerHTML === true) {
+        hungherHTML.style.opacity = 1;
+        hungherPlayer = Math.round(hungherPlayer);
+        hungherHTML.innerHTML = "&#127789;: " + hungherPlayer;
+
+        thirstHTML.style.opacity = 1;
+        thirstPlayer = Math.round(thirstPlayer);
+        thirstHTML.innerHTML = "&#128166;: " + thirstPlayer;
+
+        if (stressPlayer === false) {
+            stressHTML.style.opacity = 0;
+        } else {
+            stressHTML.style.opacity = 1;
+            stressPlayer = Math.round(stressPlayer);
+            stressHTML.innerHTML = "&#129504;: " + stressPlayer;
+        }
+
+        healthHTML.style.opacity = 1;
+        healthPlayer = healthPlayer - 100;
+        if(healthPlayer === -100) {
+            healthHTML.innerHTML = "&#128128;";
+        } else {
+            healthHTML.innerHTML = "&#128151;: " + healthPlayer;
+        }
+
+        idHTML.style.opacity = 1;
+        idHTML.innerHTML = "<font color=violet>ID - </font>" + idPlayer;
+
+    } else if(EnablePlayerHTML === false) {
+        healthHTML.style.opacity = 0;
+        hungherHTML.style.opacity = 0;
+        thirstHTML.style.opacity = 0;
+        stressHTML.style.opacity = 0;
+        idHTML.style.opacity = 0;
+    }
+
+    if(event.data.hourValue === true) {
+        EnableHourHTML = true;
+    } else if(event.data.hourValue === false) {
+        EnableHourHTML = false;
+    }
+
+    if(EnableHourHTML === true) {
+        hourHTML.style.opacity = 1;
+        hourHTML.innerHTML = hourPlayer;
+    } else if(EnableHourHTML === false) {
+        hourHTML.style.opacity = 0;
+    }
+})
